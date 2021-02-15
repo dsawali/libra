d8ca8b85faadf6b8a8c89afd3d2d2036b2b99b0b
Hansen William <hansen_william@sfu.ca>
2021-02-15 14:42:54 -0800

    Added route for symbol lookup and stock quote

--- stock-express/server.js
+++ stock-express/server.js
@@ -3,6 +3,10 @@ const server = express();
 
 const PORT = 8000;
 
+const endpoints = require('./src');
+
+server.use('/', endpoints);
+
 server.listen(PORT, () => {
-    console.log('listening on port:', PORT)
+  console.log('listening on port:', PORT)
 });
