107bdf3e9960ef5f487de24b750c7382d3c31741
wilhans <hansenwilliam0225@gmail.com>
2021-08-25 21:48:32 -0700

    Separate route and controller

--- stock-express/src/config/config.js
+++ stock-express/src/config/config.js
@@ -1,7 +1,7 @@
 import {} from 'dotenv/config.js';
 
 const config = {
-  baseurl: process.env.FINNHUB_BASE_URL,
+  finnhubBaseurl: process.env.FINNHUB_BASE_URL,
   token: process.env.FINNHUB_TOKEN,
   mongoUrl: process.env.DB_URL,
 };
